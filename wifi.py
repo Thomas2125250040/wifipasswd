3e1b00cb26c2726a04a2894324dc443cbf64f8a7
Thomas2125250040 <113816448+Thomas2125250040@users.noreply.github.com>
2023-02-26 18:42:25 +0700

    I made the code to be shorter.
    The code is shorter but have the sane function as before.

--- wifi.py
+++ wifi.py
@@ -1,35 +1,29 @@
 import subprocess
 
-meta_data = subprocess.check_output(['netsh','wlan','show','profiles'])
-data = meta_data.decode('utf-8',errors='backslashreplace')
+data = subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8',errors='backslashreplace').split("\n")
 
-data = data.split("\n")
+profiles = [a.split(":")[1][1:-1] for a in data if "All User Profile" in a]
 
-profiles = []
-
-for i in data:
-    if "All User Profile" in i:
-        i = i.split(":")
-        i = i[1]
-        i = i[1:-1]
-        profiles.append(i)
-
-print("{:<30}| {:<}".format("Wi-Fi Name", "Password"))
-print("--------------------------------------------------------")
+print("{:<30}| {:<}\n{}".format("Wi-Fi Name", "Password","-"*50))
 
 for i in profiles:
-    try:
-        a = ['netsh','wlan','show','profiles',i,'key=clear']
-        results = subprocess.check_output(['netsh','wlan','show','profiles',i,'key=clear'])
-        results = results.decode('utf-8', errors='backslashreplace')
-        results = results.split('\n')
-        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
 
+    try:
+
+        a = ['netsh','wlan','show','profiles',i,'key=clear']
+
+        results = subprocess.check_output(['netsh','wlan','show','profiles',i,'key=clear']).decode('utf-8', errors='backslashreplace').split('\n')
+        
+        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+        
         try:
+
             print("{:<30}| {:<}".format(i, results[0]))
 
         except IndexError:
+
             print("{:<30}| {:<}".format(i, ""))
 
     except subprocess.CalledProcessError:
+        
         print("Encoding Error Occured")
